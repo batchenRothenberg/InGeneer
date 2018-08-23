2a469721de251e0f2557059f3f70f14d0c90c921
batchen rothenberg <batcheni89@gmail.com>
2018-08-23 11:59:45 +0300

    Reformat file. _MINF,_INF to MINF,INF

--- interval.py
+++ interval.py
@@ -1,17 +1,18 @@
 import sys
 
-_MINF = "minf"
-_INF = "inf"
+MINF = "minf"
+INF = "inf"
 _UBOT = u'\u27D8'
 _UTOP = u'\u27D9'
 _UINF = u'\u221E'
 _UIN = u'\u220A'
 
+
 class Interval:
 
-    def __init__(self,var,low,high):
-        assert(isinstance(low,int) or low == _MINF)
-        assert(isinstance(high,int) or high == _INF)
+    def __init__(self, var, low, high):
+        assert (isinstance(low, int) or low == MINF)
+        assert (isinstance(high, int) or high == INF)
         self.low = low
         self.high = high
         self.var = var
@@ -19,9 +20,9 @@ class Interval:
     def __str__(self):
         l = str(self.low)
         h = str(self.high)
-        if self.low == _MINF:
+        if self.low == MINF:
             l = "-" + _UINF
-        if self.high == _INF:
+        if self.high == INF:
             h = _UINF
         if self.is_bottom():
             return self.var + _UIN + u'\u27C2'
@@ -31,32 +32,32 @@ class Interval:
         return str(self)
 
     def is_bottom(self):
-        return isinstance(self.low,int) and isinstance(self.high,int) and self.high < self.low
+        return isinstance(self.low, int) and isinstance(self.high, int) and self.high < self.low
 
     def is_top(self):
-        return self.low == _MINF and self.high == _INF
+        return self.low == MINF and self.high == INF
 
-    def is_inf(self):
-        return self.low == _MINF or self.high == _INF
+    def is_infinite(self):
+        return self.low == MINF or self.high == INF
 
     def __len__(self):
         if self.is_bottom():
             return 0
         if isinstance(self.low, int) and isinstance(self.high, int):
             return self.high - self.low
-        assert(self.is_inf())
+        assert (self.is_infinite())
         return sys.maxsize
-        
+
     def len_string(self):
-        if self.is_inf():
-            return _INF
+        if self.is_infinite():
+            return INF
         else:
             return len(self)
 
 
 class IntervalSet:
 
-    def __init__(self,intervals):
+    def __init__(self, intervals):
         self.dict = {i.var: i for i in intervals if not i.is_top()}
 
     def __str__(self):
@@ -64,7 +65,7 @@ class IntervalSet:
             return _UTOP
         if self.is_bottom():
             return _UBOT
-        res =""
+        res = ""
         for v in self.dict.values():
             res = res + str(v) + " "
         return res
