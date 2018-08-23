3c4fe0b285e39416e00cb13fabeeed8b26fff2a3
batchen rothenberg <batcheni89@gmail.com>
2018-08-23 10:29:53 +0300

    Added get_vars_and_coefficients(f) where f is a linear combination

--- z3getvars.py
+++ z3getvars.py
@@ -1,13 +1,17 @@
 from z3 import *
 
+
 # Wrapper for allowing Z3 ASTs to be stored into Python Hashtables.
 class AstRefKey:
     def __init__(self, n):
         self.n = n
+
     def __hash__(self):
         return self.n.hash()
+
     def __eq__(self, other):
         return self.n.eq(other.n)
+
     def __repr__(self):
         return str(self.n)
 
@@ -19,12 +23,35 @@ def askey(n):
 
 def get_vars(f):
     r = set()
+
     def collect(f):
-      if is_const(f):
-          if f.decl().kind() == Z3_OP_UNINTERPRETED and not askey(f) in r:
-              r.add(askey(f))
-      else:
-          for c in f.children():
-              collect(c)
+        if is_const(f):
+            if f.decl().kind() == Z3_OP_UNINTERPRETED and not askey(f) in r:
+                r.add(askey(f))
+        else:
+            for c in f.children():
+                collect(c)
+
+    collect(f)
+    return r
+
+
+def get_vars_and_coefficients(f):
+    """Returns a dictionary mapping each variable to its coefficient in f
+    (f should be a linear combination of int variables)"""
+    r = {}
+
+    def collect(f):
+        if is_mul(f):
+            assert is_int_value(f.arg(0))
+            assert f.arg(1).decl().kind() == Z3_OP_UNINTERPRETED
+            if not askey(f.arg(1)) in r.keys():
+                r[askey(f.arg(1))]=f.arg(0)
+        elif f.decl().kind() == Z3_OP_UNINTERPRETED and not askey(f) in r:
+            r[askey(f)] = 1
+        else:
+            for c in f.children():
+                collect(c)
+
     collect(f)
     return r
