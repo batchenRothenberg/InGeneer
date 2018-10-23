29e5c72ca4620e5da7be6bff8eb11e4174b3b084
batchen <batcheni89@gmail.com>
2018-10-23 10:47:09 +0300

    rearrange: all methods are now part of the StrenghenedFormula class

--- formula_strengthener.py
+++ formula_strengthener.py
@@ -22,6 +22,91 @@ class StrenghenedFormula():
     def add_interval(self, var, interval):
         self.interval_set.add_interval(var, interval)
 
+    def _strengthen_conjunct(self, conjunct, model):
+        if is_not(conjunct):
+            self._strengthen_conjunct(negate_condition(conjunct.arg(0)), model)
+        elif is_binary_boolean(conjunct):
+            lhs, rhs, lhs_value, rhs_value, op = evaluate_binary_expr(conjunct, model)
+            self._strengthen_binary_boolean_conjunct(lhs, lhs_value, rhs_value, op, model)
+        else:
+            self.add_unsimplified_demand(conjunct)
+
+    def _add_interval_for_binary_boolean(self, var, var_value, rhs_value, op):
+        if op == Z3_OP_LE:
+            self.add_interval(str(var), Interval(MINF, rhs_value))
+        elif op == Z3_OP_LT:
+            self.add_interval(str(var), Interval(MINF, rhs_value - 1))
+        elif op == Z3_OP_GE:
+            self.add_interval(str(var), Interval(rhs_value, INF))
+        elif op == Z3_OP_GT:
+            self.add_interval(str(var), Interval(rhs_value + 1, INF))
+        elif op == Z3_OP_EQ:
+            self.add_interval(str(var), Interval(rhs_value, rhs_value))
+        elif op == Z3_OP_DISTINCT:
+            self._add_interval_for_binary_boolean(var, var_value, rhs_value,
+                                             self._replace_distinct_with_ineq(var_value, rhs_value))
+
+    @staticmethod
+    def _replace_distinct_with_ineq(lhs_value, rhs_value):
+        if lhs_value > rhs_value:
+            return Z3_OP_GT
+        else:
+            assert lhs_value < rhs_value
+            return Z3_OP_LT
+
+    def _strengthen_add(self, lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model):
+        # print("strengthening add: ",lhs)
+        if op == Z3_OP_LE:
+            lhs_value = lhs_arg0_val + lhs_arg1_val
+            diff = rhs_value - lhs_value
+            assert diff >= 0
+            first_addition = diff // 2
+            second_addition = diff - first_addition
+            self._strengthen_binary_boolean_conjunct(lhs_arg0, lhs_arg0_val, lhs_arg0_val + first_addition, op, model)
+            self._strengthen_binary_boolean_conjunct(lhs_arg1, lhs_arg1_val, lhs_arg1_val + second_addition, op, model)
+
+    def _strengthen_mul_by_constant(self, lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model):
+        if is_int_value(lhs_arg0):
+            constant = lhs_arg0_val
+            var = lhs_arg1
+            var_value = lhs_arg1_val
+        else:
+            assert is_int_value(lhs_arg1)
+            constant = lhs_arg1_val
+            var = lhs_arg0
+            var_value = lhs_arg0_val
+        if op == Z3_OP_DISTINCT:
+            ineq_op = self._replace_distinct_with_ineq(constant * var_value, rhs_value)
+            self._strengthen_mul_by_constant(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, ineq_op, rhs_value, model)
+            return
+        if constant > 0:
+            is_round_up = (op == Z3_OP_GE or op == Z3_OP_GT)
+            self._strengthen_binary_boolean_conjunct(var, var_value, rhs_value // constant + is_round_up, op, model)
+        elif constant < 0:
+            reversed_op = reverse_operator(op)
+            is_round_up = (reversed_op == Z3_OP_GE or reversed_op == Z3_OP_GT)
+            self._strengthen_binary_boolean_conjunct(var, var_value, rhs_value // constant + is_round_up, reversed_op, model)
+
+    def _strengthen_binary_boolean_conjunct(self, lhs, lhs_value, rhs_value, op, model):
+        print("lhs " + str(lhs) + " rhs_value: " + str(rhs_value))
+        if is_const(lhs):
+            self._add_interval_for_binary_boolean(lhs, lhs_value, rhs_value, op)
+        elif is_app_of(lhs, Z3_OP_UMINUS):
+            arg0 = lhs.arg(0)
+            self._strengthen_binary_boolean_conjunct(arg0, -lhs_value, -rhs_value, reverse_operator(op), model)
+        elif is_binary(lhs):
+            lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, lhs_op = evaluate_binary_expr(lhs, model)
+            if lhs_op == Z3_OP_ADD:
+                self._strengthen_add(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model)
+            elif lhs_op == Z3_OP_SUB:
+                self._strengthen_add(lhs_arg0, -lhs_arg1, lhs_arg0_val, -lhs_arg1_val, op, rhs_value, model)
+            elif lhs_op == Z3_OP_MUL and (is_int_value(lhs_arg0) or is_int_value(lhs_arg1)):
+                self._strengthen_mul_by_constant(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model)
+            else:
+                self.add_unsimplified_demand(build_binary_expression(lhs, IntVal(rhs_value), op))
+        else:
+            self.add_unsimplified_demand(build_binary_expression(lhs, IntVal(rhs_value), op))
+
 
 def strengthen(f, model):
     res = StrenghenedFormula()
@@ -29,96 +114,7 @@ def strengthen(f, model):
     print("f_as_and: "+str(f_as_and))
     if is_and(f_as_and):
         for c in f_as_and.children():
-            _strengthen_conjunct(c, model, res)
+            res._strengthen_conjunct(c, model)
     else: # f_is_and is an atomic boolean constraint
-        _strengthen_conjunct(f_as_and, model, res)
+        res._strengthen_conjunct(f_as_and, model)
     return res
-
-
-def _strengthen_conjunct(conjunct, model, res):
-    if is_not(conjunct):
-        _strengthen_conjunct(negate_condition(conjunct.arg(0)), model, res)
-    elif is_binary_boolean(conjunct):
-        lhs, rhs, lhs_value, rhs_value, op = evaluate_binary_expr(conjunct, model)
-        _strengthen_binary_boolean_conjunct(lhs, lhs_value, rhs_value, op, model, res)
-    else:
-        res.add_unsimplified_demand(conjunct)
-
-
-def _add_interval_for_binary_boolean(var, var_value, rhs_value, op, res):
-    if op == Z3_OP_LE:
-        res.add_interval(str(var), Interval(MINF, rhs_value))
-    elif op == Z3_OP_LT:
-        res.add_interval(str(var), Interval(MINF, rhs_value - 1))
-    elif op == Z3_OP_GE:
-        res.add_interval(str(var), Interval(rhs_value, INF))
-    elif op == Z3_OP_GT:
-        res.add_interval(str(var), Interval(rhs_value + 1, INF))
-    elif op == Z3_OP_EQ:
-        res.add_interval(str(var), Interval(rhs_value, rhs_value))
-    elif op == Z3_OP_DISTINCT:
-        _add_interval_for_binary_boolean(var, var_value, rhs_value, _replace_distinct_with_ineq(var_value, rhs_value), res)
-
-
-def _replace_distinct_with_ineq(lhs_value, rhs_value):
-    if lhs_value > rhs_value:
-        return Z3_OP_GT
-    else:
-        assert lhs_value < rhs_value
-        return Z3_OP_LT
-
-
-def _strengthen_add(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model, res):
-    # print("strengthening add: ",lhs)
-    if op == Z3_OP_LE:
-        lhs_value = lhs_arg0_val + lhs_arg1_val
-        diff = rhs_value - lhs_value
-        assert diff >= 0
-        first_addition = diff // 2
-        second_addition = diff - first_addition
-        _strengthen_binary_boolean_conjunct(lhs_arg0, lhs_arg0_val, lhs_arg0_val + first_addition, op, model, res)
-        _strengthen_binary_boolean_conjunct(lhs_arg1, lhs_arg1_val, lhs_arg1_val + second_addition, op, model, res)
-
-
-def _strengthen_mul_by_constant(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model, res):
-    if is_int_value(lhs_arg0):
-        constant = lhs_arg0_val
-        var = lhs_arg1
-        var_value = lhs_arg1_val
-    else:
-        assert is_int_value(lhs_arg1)
-        constant = lhs_arg1_val
-        var = lhs_arg0
-        var_value = lhs_arg0_val
-    if op == Z3_OP_DISTINCT:
-        ineq_op = _replace_distinct_with_ineq(constant * var_value, rhs_value)
-        _strengthen_mul_by_constant(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, ineq_op, rhs_value, model, res)
-        return
-    if constant > 0:
-        is_round_up = (op == Z3_OP_GE or op == Z3_OP_GT)
-        _strengthen_binary_boolean_conjunct(var,var_value,rhs_value//constant+is_round_up,op,model,res)
-    elif constant < 0:
-        reversed_op = reverse_operator(op)
-        is_round_up = (reversed_op == Z3_OP_GE or reversed_op == Z3_OP_GT)
-        _strengthen_binary_boolean_conjunct(var,var_value,rhs_value//constant+is_round_up,reversed_op,model,res)
-
-
-def _strengthen_binary_boolean_conjunct(lhs, lhs_value, rhs_value, op, model, res):
-    print("lhs " + str(lhs) + " rhs_value: " + str(rhs_value))
-    if is_const(lhs):
-        _add_interval_for_binary_boolean(lhs, lhs_value, rhs_value, op, res)
-    elif is_app_of(lhs, Z3_OP_UMINUS):
-        arg0 = lhs.arg(0)
-        _strengthen_binary_boolean_conjunct(arg0, -lhs_value, -rhs_value, reverse_operator(op), model, res)
-    elif is_binary(lhs):
-        lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, lhs_op = evaluate_binary_expr(lhs, model)
-        if lhs_op == Z3_OP_ADD:
-            _strengthen_add(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model, res)
-        elif lhs_op == Z3_OP_SUB:
-            _strengthen_add(lhs_arg0, -lhs_arg1, lhs_arg0_val, -lhs_arg1_val, op, rhs_value, model, res)
-        elif lhs_op == Z3_OP_MUL and (is_int_value(lhs_arg0) or is_int_value(lhs_arg1)):
-            _strengthen_mul_by_constant(lhs_arg0, lhs_arg1, lhs_arg0_val, lhs_arg1_val, op, rhs_value, model, res)
-        else:
-            res.add_unsimplified_demand(build_binary_expression(lhs, IntVal(rhs_value), op))
-    else:
-        res.add_unsimplified_demand(build_binary_expression(lhs,IntVal(rhs_value),op))
