057864b73a3f5a717ef375705b8152124608928b
batchen <batcheni89@gmail.com>
2019-06-22 12:28:19 +0300

    interval_domain: implemented (untested)

--- interval_domain.py
+++ interval_domain.py
@@ -1,4 +1,6 @@
 from domain import Domain
+from precise_domain import PreciseDomain
+from formula_strengthener import StrenghenedFormula,strengthen
 from stmt import *
 from z3_utils import *
 from interval import *
@@ -6,59 +8,68 @@ from interval import *
 
 class IntervalDomain(Domain):
 
-    def do_step(self,I, st, model):
-        if st.is_assignment():
-            return interval_assignment_pre_step(I, st)
+    ''' Class implementing abstract 'domain' class
+        for formulas consisting of an IntervalSet (upper and lower bounds for variables)
+        and a precise formula - manipulated using the precise domain (weakest-precondition)
+    '''
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def do_step(self, f, stmt, model):
+        if stmt.is_assignment():
+            return self._assignment_pre_step(f, stmt, model)
         else:
-            assert st.is_condition()
-            cond = st.expr
-            if is_and(cond):
-                return Interval.intersection([interval_condition_pre_step(I, c) for c in cond.chidren()])
-            elif is_or(cond):
-                # arbitrarily choose to satisfy the first disjunct
-                return interval_condition_pre_step(I, cond.arg(0))
-            elif is_not(cond):
-                # arbitrarily choose to satisfy the first disjunct
-                neg_cond = negate_condition(cond)
-                return interval_condition_pre_step(I, neg_cond)
-            else:
-                return interval_condition_pre_step(I, cond)
+            assert stmt.is_condition()
+            cond = stmt.expr
+            return self._condition_pre_step(f, cond, model)
 
     def does_step_lead_to_state_in_model(self, f, stmt, model):
-        # todo: IMPLEMENT!
-        return True
+        if model is None:
+            return True
+        else:
+            if stmt.is_assignment():
+                assigned_var = stmt.lhs
+                assignment_expr = stmt.rhs
+                new_value = model.evaluate(assignment_expr)
+                new_model = update_model(model,[(assigned_var,new_value)])
+                return f.interval_set.is_var_in_range(assigned_var,new_value) and new_model.evaluate(And(f.unsimplified_demands))
+            else:
+                assert stmt.is_condition()
+                cond = stmt.expr
+                return model.evaluate(cond)
 
-    def is_bottom(self, I):
-        return I.is_bottom()
+    def is_bottom(self, f):
+        return f.is_bottom()
 
-    def is_top(self, I):
-        return I.is_top()
+    def is_top(self, f):
+        return f.is_top()
 
     def intersection(self, formulas):
-        return IntervalSet.intersection(formulas)
+        return StrenghenedFormula.intersection(formulas)
 
     def get_top(self):
-        return IntervalSet.get_top()
+        return StrenghenedFormula.get_top(self.debug)
 
     def get_bottom(self):
-        return IntervalSet.get_bottom()
+        return StrenghenedFormula.get_bottom(self.debug)
 
     def choose(self, formulas, model):
         chosen_indices = [i for i in range(len(formulas))]
         unchosen_indices = []
         return chosen_indices, unchosen_indices
 
+    def _assignment_pre_step(self, f, stmt, model):
+        assigned_var = stmt.lhs
+        assignment_expr = stmt.rhs
+        if assigned_var not in f.interval_set:
+            return f
+        else:
+            var_interval = f.interval_set.get_interval(assigned_var)
+            f_copy = IntervalSet.get_top().intersect(f)
+            f_copy.delete_interval(assigned_var)
+            cond = And(var_interval.low <= assignment_expr, assignment_expr <= var_interval.high)
+            return self._condition_pre_step(f_copy, cond, model)
 
-def interval_assignment_pre_step(I, st):
-    return I
-
-
-def interval_condition_pre_step(I, cond):
-    # simplified_expr = simplify(st.expr,arith_lhs=True)
-    # linear_combination = simplified_expr.arg(0)
-    # var_to_coeff_dict = get_vars_and_coefficients(linear_combination)
-    # constant = simplified_expr.arg(1).as_long()
-    # upper_bound = I.max_value(linear_combination)
-    # lower_bound = I.min_value(linear_combination)
-    # print(linear_combination,constant,var_to_coeff_dict)
-    return I
+    def _condition_pre_step(self, f, cond, model):
+        strengthened_condition =  strengthen(cond,model,self.debug)
+        return StrenghenedFormula.intersection([f,strengthened_condition])
